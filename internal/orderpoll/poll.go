584632f69d0e73f5389929fe64f6a982c028a098
Eduard Tungatarov <eduard.tungatarov@yandex.ru>
2025-07-10 23:25:09 +0500

    iter1 ref

--- internal/orderpoll/poll.go
+++ internal/orderpoll/poll.go
@@ -54,84 +54,93 @@ func New(log *zap.SugaredLogger, cfg config.Config, orderSrv OrderService, clien
 }
 
 func (o *OrderPoll) Run(ctx context.Context) error {
-	pollCtx, cancel := context.WithCancel(ctx)
+	ctx, cancel := context.WithCancel(ctx)
+
 	errChan := make(chan error, o.workerNum+1)
 	orderCh := make(chan OrderChValue, o.workerNum)
 
-	go func(ch chan<- OrderChValue) {
-		defer close(ch)
-		for {
-			orders, err := o.orderSrv.FindByInProgressStatuses(pollCtx)
-			if err != nil {
-				errChan <- fmt.Errorf("orderSrv.FindByInProgressStatuses: %w", err)
-				return
-			}
-
-			for _, v := range orders {
-				select {
-				case ch <- OrderChValue{
-					OrderNumber: v.OrderNumber,
-					UserID:      v.UserID,
-				}:
-				case <-pollCtx.Done():
-					return
-				}
-			}
-
-			select {
-			case <-time.After(o.sleepTime):
-			case <-pollCtx.Done():
-				return
-			}
+	go func() {
+		err := o.RunReader(ctx, orderCh)
+		if err != nil {
+			errChan <- fmt.Errorf("orderSrv.UpdateOrder: %w", err)
 		}
-	}(orderCh)
+	}()
 
 	for i := 0; i < o.workerNum; i++ {
-		go func(ch <-chan OrderChValue) {
-			for {
-				select {
-				case orderChV, ok := <-orderCh:
-					if !ok {
-						return
-					}
-					//
-
-					resp, err := o.client.GetOrder(orderChV.OrderNumber)
-					if err != nil {
-						var nonOkErr *accrual.NonOkError
-						if ok := errors.As(err, &nonOkErr); ok {
-							if nonOkErr.Code == http.StatusNoContent {
-								err = o.orderSrv.UpdateOrder(pollCtx, orderChV.UserID, orderChV.OrderNumber, order.StatusInvalid, 0)
-								if err != nil {
-									errChan <- fmt.Errorf("orderSrv.UpdateOrder: %w", err)
-									return
-								}
-							}
-						}
-						o.log.Error("o.client.GetOrder net err", err)
-						return
-					}
-
-					err = o.orderSrv.UpdateOrder(pollCtx, orderChV.UserID, orderChV.OrderNumber, resp.Status, int(resp.Accrual*100))
-					if err != nil {
-						errChan <- fmt.Errorf("orderSrv.UpdateOrder: %w", err)
-						return
-					}
-
-					//
-				case <-pollCtx.Done():
-					return
-				}
+		go func() {
+			err := o.RunWorker(ctx, orderCh)
+			if err != nil {
+				errChan <- fmt.Errorf("o.RunWorker: %w", err)
 			}
-		}(orderCh)
+		}()
 	}
 
 	select {
 	case err := <-errChan:
 		cancel()
 		return err
-	case <-pollCtx.Done():
+	case <-ctx.Done():
 		cancel()
 		return nil
+	}
+}
+
+func (o *OrderPoll) RunReader(ctx context.Context, ch chan<- OrderChValue) error {
+	defer close(ch)
+	for {
+		orders, err := o.orderSrv.FindByInProgressStatuses(ctx)
+		if err != nil {
+			return err
+		}
+
+		for _, v := range orders {
+			select {
+			case ch <- OrderChValue{
+				OrderNumber: v.OrderNumber,
+				UserID:      v.UserID,
+			}:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+
+		select {
+		case <-time.After(o.sleepTime):
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+func (o *OrderPoll) RunWorker(ctx context.Context, ch <-chan OrderChValue) error {
+	for {
+		select {
+		case orderChV, ok := <-ch:
+			if !ok {
+				return nil
+			}
+
+			resp, err := o.client.GetOrder(orderChV.OrderNumber)
+			if err != nil {
+				var nonOkErr *accrual.NonOkError
+				if ok := errors.As(err, &nonOkErr); ok {
+					if nonOkErr.Code == http.StatusNoContent {
+						err = o.orderSrv.UpdateOrder(ctx, orderChV.UserID, orderChV.OrderNumber, order.StatusInvalid, 0)
+						if err != nil {
+							return err
+						}
+						continue
+					}
+				}
+				return err
+			}
+
+			err = o.orderSrv.UpdateOrder(ctx, orderChV.UserID, orderChV.OrderNumber, resp.Status, int(resp.Accrual*100))
+			if err != nil {
+				return err
+			}
+		case <-ctx.Done():
+			return nil
+		}
 	}
 }
