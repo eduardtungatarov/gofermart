56141f90e7d758f3a730acbd74437f51a8a08661
Eduard Tungatarov <eduard.tungatarov@yandex.ru>
2025-07-16 18:01:24 +0500

    Лучше использовать IDENTITY вместо SERIAL

--- migrations/20250703165909_create_tables.sql
+++ migrations/20250703165909_create_tables.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     login VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
@@ -9,34 +9,34 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE INDEX IF NOT EXISTS idx_users_login ON users(login);
 
 CREATE TABLE IF NOT EXISTS orders (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id INTEGER NOT NULL,
     order_number VARCHAR(255) UNIQUE NOT NULL,
     status VARCHAR(255) NOT NULL,
     accrual INTEGER NOT NULL,
     uploaded_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
-    );
+);
 
 CREATE INDEX IF NOT EXISTS idx_orders_order_number ON orders(order_number);
 CREATE INDEX IF NOT EXISTS idx_orders_user_id ON orders(user_id);
 CREATE INDEX IF NOT EXISTS idx_orders_status ON orders(status);
 
 CREATE TABLE IF NOT EXISTS balance (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id INTEGER UNIQUE NOT NULL,
     current INTEGER NOT NULL DEFAULT 0 CHECK (current >= 0),
     withdrawn INTEGER NOT NULL DEFAULT 0 CHECK (withdrawn >= 0)
-    );
+);
 
 CREATE INDEX IF NOT EXISTS idx_balance_user_id ON balance(user_id);
 
 CREATE TABLE IF NOT EXISTS withdrawals (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id INTEGER NOT NULL,
     order_number VARCHAR(255) NOT NULL,
     sum INTEGER NOT NULL,
     processed_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
-    );
+);
 
 CREATE INDEX IF NOT EXISTS idx_withdrawals_user_id ON withdrawals(user_id);
 -- +goose StatementEnd
